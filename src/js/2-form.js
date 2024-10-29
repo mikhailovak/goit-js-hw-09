1d5113a3c0930a4825b8520009d4b2200b1b9c37
Ksenia Melnyk <melnyksenia@gmail.com>
2024-10-29 08:53:42 +0100

    1st pass

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,47 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+const formData = {
+  email: "",
+  message: ""
+};
+
+const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector("input");
+const messageInput = form.querySelector("textarea");
+
+const STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener('input', onInput);
+form.addEventListener('submit', onSubmit);
+
+function onInput(event) {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateFormData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || "";
+    formData.message = parsedData.message || "";
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+}
+
+populateFormData();
+
+function onSubmit(event) {
+  event.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+    console.log("Form Data:", formData);
+
+  formData.email = "";
+  formData.message = "";
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
